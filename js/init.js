d6dbb9b88f2d1ce5581110d364240dc13863c5a9
Nicolasdark <86506084+Nicolasdark@users.noreply.github.com>
2021-07-21 14:27:57 -0300

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCj-TltiLZ-XFhJC_bS636eSQ6a9bzH4hs",
+    authDomain: "fir-6fe09.firebaseapp.com",
+    projectId: "fir-6fe09",
+    storageBucket: "fir-6fe09.appspot.com",
+    messagingSenderId: "1081418608050",
+    appId: "1:1081418608050:web:d18ad1562c2e0a6a1c08ab",
+    measurementId: "G-NWP3GZJ2BM"
+};
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
